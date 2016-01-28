1c32aaf9e3cbd23a1b3f80221c48b52d06fb9020
saabogal <luis.alejandro.sarmiento.abogado@cms.hu-berlin.de>
2016-01-28 16:48:27 +0100

    Changes to the graphs and code

--- quiz2_p1_9/Quizz two.R
+++ quiz2_p1_9/Quizz two.R
@@ -5,28 +5,36 @@ rm(list = ls())
 # Load Packages
 library(fGarch)
 
-# Left Skewed
+# Right Skewed
 r = rsnorm(n = 1e+05, xi = 2)
 
 # Plot the distribution
-hist(r, n = 100, probability = TRUE, border = "white", col = "red", main = "Left Skewed Dist.")
+png(filename = "Quiz two a.png", height = 300)
+hist(r, n = 100, probability = TRUE, border = "white", col = "red", main = "Right Skewed Dist.")
 box()
+dev.off()
 
 # Compute the density
 d = density(r)
 lines(d, lwd = 2, col = "blue")
+dev.off()
 
-# Right Skewed distribution 
+# Left Skewed distribution 
 rr = rsnorm(n = 1e+05, xi = -2)
 
 # Plot the given distribution and correspondent Histogram
+png(filename = "Quiz two b.png", height = 300)
 hist(rr, n = 100, probability = TRUE, border = "white", col = "green", 
-     main = "Right Skewed Dist.")
+     main = "Left Skewed Dist.")
 box()
 
+dev.off()
 # Compute the density
 dr = density(rr)
 lines(dr, lwd = 2)
+
+dev.off()
+
 
 
 
